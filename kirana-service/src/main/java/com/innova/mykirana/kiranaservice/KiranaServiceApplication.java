9d8258b9e0ce3117059aa2234e7e453baac76f72
Nirupam Das <nirupam.saie@gmail.com>
2021-05-02 20:51:02 +0530

    Initial commit kirana-service

---
+++ kirana-service/src/main/java/com/innova/mykirana/kiranaservice/KiranaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innova.mykirana.kiranaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaServiceApplication.class, args);
+	}
+
+}
---
+++ kirana-service/src/test/java/com/innova/mykirana/kiranaservice/KiranaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.mykirana.kiranaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
